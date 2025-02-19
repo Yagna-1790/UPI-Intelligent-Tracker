0b162e77244316e8a73a0774b2abc45cd6daf8b1
Yagna Narayana Singh <97424657+Yagna-1790@users.noreply.github.com>
2025-02-19 16:13:19 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,25 @@ import Dashboard from './pages/Dashboard';
 import Transactions from './pages/Transactions';
 import QRScanner from './pages/QRScanner';
 import MerchantDashboard from './pages/MerchantDashboard';
+import { TransactionProvider } from './context/TransactionContext';
 
 function App() {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-50">
-        <Navbar />
-        <main className="container mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/transactions" element={<Transactions />} />
-            <Route path="/scan" element={<QRScanner />} />
-            <Route path="/merchant" element={<MerchantDashboard />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <TransactionProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50">
+          <Navbar />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/scan" element={<QRScanner />} />
+              <Route path="/merchant" element={<MerchantDashboard />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </TransactionProvider>
   );
 }
 
